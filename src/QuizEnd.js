b7a296a99a45752f075a5c6835e356aa1987d831
Gareth A <31896652+garethashby@users.noreply.github.com>
2019-03-06 16:14:29 +0000

    corrected spelling error

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -4,7 +4,7 @@ class QuizEnd extends Component {
 render () {
     return (
         <div>
-            <p>Thansk for playing!</p>
+            <p>Thanks for playing!</p>
             <a href=''>Reset Quiz</a>
         </div>
     )
